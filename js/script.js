32a3513bb771c141bd9cd6d0130b990636cdcc65
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2023-09-27 03:24:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var button = document.getElementById("meu-botao");
+var imageContainer = document.getElementById("imageContainer");
+button.addEventListener("click", () => {
+  audio.play();
+  button.style.zIndex = 0;
+  setTimeout(() => {
+    imageContainer.style.opacity = 1;
+  }, 100);
+});
